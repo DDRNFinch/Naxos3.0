b377dd35c4e6138f176df51a33664743de418581
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-13 14:10:13 +0100

    Bump Naxos cache to v19

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'naxos3-v18';
+const CACHE_NAME = 'naxos3-v19';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './url-import.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
@@ -14,12 +14,5 @@ self.addEventListener('fetch', event => {
   if (request.method !== 'GET') return;
   const url = new URL(request.url);
   if (url.origin !== self.location.origin) return;
-
-  event.respondWith(
-    caches.match(request).then(cached => cached || fetch(request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
-  );
+  event.respondWith(caches.match(request).then(cached => cached || fetch(request).then(response => { const copy = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(request, copy)); return response; }).catch(() => caches.match('./index.html'))));
 });
